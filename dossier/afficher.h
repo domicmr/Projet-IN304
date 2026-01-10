6ef50d1d5d5f3eb23659f9632463f90ad4b4b2de
domicmr <dominiquecamara2@gmail.com>
2026-01-10 22:42:37 +0100

    Create afficher.h

---
+++ dossier/afficher.h
@@ -0,0 +1,9 @@
+#ifndef AFFICHER_H
+#define AFFICHER_H
+
+#include "morphing_type.h"
+
+void afficher_image(IMAGE I, int x0, int y0);
+void afficher_deux_images(IMAGE A, IMAGE B, int espace);
+
+#endif

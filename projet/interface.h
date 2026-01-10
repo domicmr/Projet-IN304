4cc1284f6a8a1ec7acdb6ca8cfc4856fa53b2b0b
domicmr <dominiquecamara2@gmail.com>
2026-01-10 20:58:58 +0100

    Create interface.h

---
+++ projet/interface.h
@@ -0,0 +1,9 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+#include "morphing_type.h"
+
+void interface_couples_points(IMAGE gauche, IMAGE droite,
+                              int espace, const char *nom_fichier);
+
+#endif

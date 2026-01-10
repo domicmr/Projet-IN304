ac04a3cf3dafc24b61e766e32253d663dbf322c3
domicmr <dominiquecamara2@gmail.com>
2026-01-10 23:37:51 +0100

    Delete projet/interface.h

--- projet/interface.h
+++
@@ -1,9 +0,0 @@
-#ifndef INTERFACE_H
-#define INTERFACE_H
-
-#include "morphing_type.h"
-
-void interface_couples_points(IMAGE gauche, IMAGE droite,
-                              int espace, const char *nom_fichier);
-
-#endif
